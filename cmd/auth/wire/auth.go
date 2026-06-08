257161a10dcfdfec91372c25df09c72b562072fc
siwen <siwench@msn.com>
2026-06-08 18:45:43 +0800

    feat(auth-runtime): wire auth module adapters

---
+++ cmd/auth/wire/auth.go
@@ -0,0 +1,93 @@
+package wire
+
+import (
+	"fmt"
+	"sc/cmd/auth/config"
+	"sc/cmd/auth/dependencies"
+	coreuow "sc/core/uow"
+	inframetrics "sc/infrastructure/metrics"
+	"sc/modules/auth"
+	adapterout "sc/modules/auth/adapter/out"
+	"sc/modules/auth/application/define"
+	"sc/modules/auth/domain/entity"
+	"sc/modules/auth/port"
+)
+
+func Auth(cfg *config.Settings, deps dependencies.Deps) *auth.Module {
+	return auth.NewModule(buildAuthDeps(cfg, deps))
+}
+
+func buildAuthDeps(cfg *config.Settings, deps dependencies.Deps) define.Dependencies {
+	var userRepo port.UserRepository
+	if cfg.RepositoryType == "mongo" {
+		r, err := adapterout.NewMongoUserRepository(deps.MongoClient)
+		if err != nil {
+			panic(fmt.Sprintf("failed to initialize user repository: %v", err))
+		}
+		userRepo = r
+	} else {
+		r, err := adapterout.NewUserRepository(deps.FileStore, buildSeedUsers())
+		if err != nil {
+			panic(fmt.Sprintf("failed to initialize user repository: %v", err))
+		}
+		userRepo = r
+	}
+
+	var refreshTokenRepo port.RefreshTokenRepository
+	if cfg.RepositoryType == "mongo" {
+		r, err := adapterout.NewMongoRefreshTokenRepository(deps.MongoClient)
+		if err != nil {
+			panic(fmt.Sprintf("failed to initialize refresh token repository: %v", err))
+		}
+		refreshTokenRepo = r
+	} else {
+		r, err := adapterout.NewFileRefreshTokenRepository(deps.RefreshTokenStore)
+		if err != nil {
+			panic(fmt.Sprintf("failed to initialize refresh token repository: %v", err))
+		}
+		refreshTokenRepo = r
+	}
+
+	uow := deps.UnitOfWork
+	if uow == nil {
+		uow = &coreuow.NoopUnitOfWork{}
+	}
+
+	return define.Dependencies{
+		Cache:                       deps.Cache,
+		UoW:                         uow,
+		JWTSvc:                      adapterout.NewJWTServiceAdapter(deps.JWTService),
+		Metrics:                     inframetrics.NewExpvarRecorder(),
+		UserRepo:                    userRepo,
+		RefreshTokenRepo:            refreshTokenRepo,
+		EmailSender:                 buildEmailSender(deps),
+		RedirectURIAllowlist:        cfg.OAuth.RedirectURIAllowlist,
+		PostLogoutRedirectAllowlist: cfg.OAuth.PostLogoutRedirectAllowlist,
+		ScopeAllowlist:              cfg.OAuth.ScopeAllowlist,
+	}
+}
+
+func buildEmailSender(deps dependencies.Deps) port.EmailSender {
+	if deps.SMTPClient != nil {
+		return adapterout.NewSmtpEmailSender(deps.SMTPClient, deps.Config.AppBaseURL)
+	}
+	return adapterout.NewLogEmailSender()
+}
+
+func buildSeedUsers() map[string]*entity.User {
+	users := make(map[string]*entity.User)
+	for i := 1000; i < 1100; i++ {
+		u, err := entity.NewUser(entity.UserArgs{
+			Username:      fmt.Sprintf("test%d", i),
+			Nickname:      fmt.Sprintf("test%d_nickname", i),
+			Password:      "1qazXSW@",
+			Email:         fmt.Sprintf("test%d@test.io", i),
+			EmailVerified: i%2 == 0,
+		})
+		if err != nil {
+			panic(fmt.Sprintf("buildSeedUsers: %v", err))
+		}
+		users[string(u.ID)] = u
+	}
+	return users
+}
